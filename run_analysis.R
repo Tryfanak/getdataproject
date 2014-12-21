991e8719b3623d48860951f590e759becfd944fb
unknown <JREAST@ANC-W7854201152.conoco.net>
2014-12-20 18:08:20 -0900

    Tidy up comments, to make it easier to review

--- run_analysis.R
+++ run_analysis.R
@@ -1,50 +1,47 @@
-# Prior to this I have set working directory, 
-# and downloaded the dataset to the working directory
+# Note - Dataset has already been loaded to working directory
 
 library(dplyr)
 library(tidyr)
-# Reference source page, and that the counts match
 
-# Read in all the data to take a look at it
+# Read in all the data tables (high level, training and test)
 dataDir <- "./UCI HAR Dataset/"
 features <- read.table(paste(dataDir,"features.txt",sep= ""))
 activity_labels <- read.table(paste(dataDir,"activity_labels.txt",sep= ""))
-subject_test <- read.table(paste(dataDir,"test/subject_test.txt",sep= ""))
-y_test <- read.table(paste(dataDir,"test/y_test.txt",sep= ""))
-x_test <- read.table(paste(dataDir,"test/X_test.txt",sep= ""))
+
 subject_train <- read.table(paste(dataDir,"train/subject_train.txt",sep= ""))
 y_train <- read.table(paste(dataDir,"train/y_train.txt",sep= ""))
 x_train <- read.table(paste(dataDir,"train/X_train.txt",sep= ""))
 
-# Add subject names and activities to LHS of each set of feature values
-# (Safe, since rows are still in the original order)
-# 563 columns - names are meaningless - address that later
-# 
+subject_test <- read.table(paste(dataDir,"test/subject_test.txt",sep= ""))
+y_test <- read.table(paste(dataDir,"test/y_test.txt",sep= ""))
+x_test <- read.table(paste(dataDir,"test/X_test.txt",sep= ""))
+
+# Add subject names and activities to LHS of each set of observations
 x_train_full <- cbind( subject_train, y_train, x_train)
 x_test_full <- cbind( subject_test, y_test, x_test)
 
-# Now combine the test and train data sets
-# (Safe, because they have all the same columns, in the same order)
-# Result has 10299 rows, which matches data set at UCI Repository
+# Combine the test and train data sets
 x_combine_full <- rbind(x_train_full, x_test_full)
 
-# Set the column names to be meaningful
-# Pre-process with make.names to clean up illegal characters and duplicates
-# (None of the mean and std columns are duplicates, so just housekeeping)
-colnames(x_combine_full) <- c("subject","activity_id", make.names(features$V2, unique = TRUE))
+# Add on column names (subject and activity_id, plus all the feature names)
+colnames(x_combine_full) <- c("subject","activity_id", 
+                                 make.names(features$V2, unique = TRUE))
 colnames(activity_labels) <- c("activity_id", "activity")
 
-# Make sets into dplyr dataframe tables, ready to work with dplyr functions
+# Convert to dplyr dataframe table, ready to work with dplyr functions
 xdata <- tbl_df(x_combine_full)
 
-# Filter columns, to keep only the mean and std columns we want 
-# then melt the remaining columns
-# then group by subject/activity/variable and average
+# Used piped dplyr functions to process into a tall, tidy, data set
+    # add meaningful activity names
 by_obs <- inner_join( xdata, activity_labels) %>%
+    #  select only the features with mean() and standard() in
     select(subject, activity, contains(".mean.."), contains(".std..")) %>%
+    #  melt into a tall dataset with one row per subject/activity/feature/obs
     gather( variable, value, -c(subject,activity) ) %>%
+    #  calculate average values for each subject/activity/feature
     group_by(subject, activity, variable) %>%
     summarize(average=mean(value)) %>%
+    #  clean up the feature names to read better in the tidy data set
     mutate( variable = gsub("BodyBody","Body",variable, fixed = TRUE))%>%
     mutate( variable = gsub("tBody","timeBody",variable, fixed = TRUE))%>%
     mutate( variable = gsub("fBody","freqBody",variable, fixed = TRUE))%>%
@@ -52,10 +49,13 @@ by_obs <- inner_join( xdata, activity_labels) %>%
     mutate( variable = gsub("fGravity","freqGravity",variable, fixed = TRUE))%>%
     mutate( variable = gsub("..","",variable, fixed = TRUE))%>%
     mutate( variable = gsub(".","_",variable, fixed = TRUE))%>%
+    #  sort by subject/activity/feature
     arrange( subject, activity, variable) 
     
-# Use spread to put all 68 variables back into columns
+# Convert back into a wide data set, with all mean/std features on one line
 result <- spread( data.frame(by_obs), variable, average)
 
+# Write out the result in the required format
 write.table(result,"tidydata.txt",row.name=FALSE)
-## tidydata <- read.table("tidydata.txt", header=TRUE )
+
+# To read back, use 'tidydata <- read.table("tidydata.txt", header=TRUE )'
